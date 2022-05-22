35a627ae6387ad6ab4a9fafeafdf39b840b4962f
manas5996 <manas5996@gmail.com>
2022-05-22 14:36:11 +0530

    array code

--- array.cpp
+++ array.cpp
@@ -4,10 +4,7 @@ using namespace std;
 
 int main()
 {
-    int A[5];
-    A[0] = 12;
-    A[1] = 15;
-    A[2] = 25;
+    int A[5] = {2, 4, 6, 8, 10};
 
     cout<<sizeof(A)<<endl;
     cout<<A[1]<<endl;
