9639b041cdb6e30731dccbe875faefd29d9d646c
manas5996 <manas5996@gmail.com>
2022-05-22 14:42:37 +0530

    array code

--- array.cpp
+++ array.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-    int A[5] = {2, 4, 6, 8, 10};
+    int A[10] = {2, 4, 6, 8, 10, 12};
 
-    cout<<sizeof(A)<<endl;
-    cout<<A[1]<<endl;
-    printf("%d\n", A[2]);
+    for(int i = 0; i < 10; i++)
+    {
+        cout<<A[i]<<endl;
+    }
 
     return 0;
 }
