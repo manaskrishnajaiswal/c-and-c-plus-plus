c3b1a9d9790802db986f261a9d5be0ef9ee52d71
manas5996 <manas5996@gmail.com>
2022-05-23 13:28:31 +0530

    code commit

---
+++ basic concepts/function.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
+
+int main()
+{
+    int x, y, z;
+    x = 10; y = 20;
+
+    z = add(x, y);
+    printf("Sum is %d.", z);
+
+    return 0;
+}
