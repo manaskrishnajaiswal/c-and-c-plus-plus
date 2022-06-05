b1c77d09875fd6cd8a26b0b6c950267a977590cf
manas5996 <manas5996@gmail.com>
2022-06-05 17:04:10 +0530

    code commit

--- basic concepts/object_orient_program.cpp
+++ basic concepts/object_orient_program.cpp
@@ -3,40 +3,42 @@
 
 using namespace std;
 
-struct rectangle
+class rectangle
 {
+public:
     int length;
     int breadth;
+
+void initialise(int l, int b)
+{
+    length=l;
+    breadth=b;
+}
+
+int area()
+{
+    return length*breadth;
+}
+
+int perimeter()
+{
+    return 2*(length+breadth);
+}
+
 };
-
-void initialise(struct rectangle *r, int l, int b)
-{
-    r->length=l;
-    r->breadth=b;
-}
-
-int area(struct rectangle r)
-{
-    return r.length*r.breadth;
-}
-
-int perimeter(struct rectangle r)
-{
-    return 2*(r.length+r.breadth);
-}
 
 int main()
 {
-    rectangle r={0, 0};
+    rectangle r;
 
     int l,b;
     printf("Enter Length and Breadth:");
     cin>>l>>b;
 
-    initialise(&r, l, b);
+    r.initialise(l, b);
 
-    int a=area(r);
-    int peri=perimeter(r);
+    int a=r.area();
+    int peri=r.perimeter();
 
     printf("Area=%d\nPerimeter=%d\n", a, peri);
 
