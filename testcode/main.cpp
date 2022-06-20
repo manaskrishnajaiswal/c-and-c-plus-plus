61cc62374e2e3b68cabb513b05be1422f8a7f57f
manas5996 <manas5996@gmail.com>
2022-06-20 19:21:48 +0530

    code commit

--- testcode/main.cpp
+++ testcode/main.cpp
@@ -1,16 +1,76 @@
 #include<stdio.h>
 #include<math.h>
+#include<iostream>
 
-void count(int n) {
-    static int d = 1; 
-    printf("%d", n); 
-    printf("%d", d); 
-    d++; 
-    if (n > 1) count (n-1); 
-    printf("%d", d);
-} 
+using namespace std;
 
 int main () {
-    count(3);
+    int test;
+    cin>>test;
+    int i_length, j_length;
+    while(test>0){
+        cin>>i_length>>j_length;
+        char table[i_length][j_length];
+        for(int i = 0; i < i_length; i++) {
+            for(int j = 0; j < j_length; j++) {
+                cin>>table[i][j];
+            } 
+        }
+
+        int B_V_counter = 1, B_V_max = 0;
+        for(int j = 1; j <= j_length; j++) {
+            for(int i = 1; i <= i_length; i++) {            
+                if(table[i][j-1] == '#' && table[i-1][j-1] == '#') {             
+                    B_V_counter++;
+                    if(B_V_counter > B_V_max) {
+                        B_V_max = B_V_counter;
+                    }                
+                    // printf("%d", B_V_counter);
+                }
+                else {
+                    B_V_counter = 1;
+                }
+            }
+        }
+
+        // printf("%d", B_V_max);
+
+
+        int B_H_counter = 1, B_H_max = 0;
+        for(int i = 1; i <= i_length; i++) {
+            for(int j = 1; j <= j_length; j++) {
+                if(table[i-1][j] == '#' && table[i-1][j-1] == '#') {              
+                    B_H_counter++;
+                    if(B_H_counter > B_H_max) {
+                        B_H_max = B_H_counter;
+                    } 
+                    // printf("%d", B_H_counter);
+                }
+                else {
+                    B_H_counter = 1;
+                }
+            }
+        }
+
+        // printf("%d", B_H_max);
+        int z = 1
+        if(B_V_max == 0 && B_H_max == 0) {
+            printf("%d", z);
+        }
+        else {
+                if(B_V_max > B_H_max) {
+                printf("%d\n", B_V_max);
+            }
+            else {
+                printf("%d\n", B_H_max);
+            }
+        }
+
+        
+
+        test--;
+    }
+    
+
     return 0;
 }
---
+++ testcode/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+if(B_V_max == 1 && B_H_max == 1) {
+            printf(0);
+        }
+        else {
+                if(B_V_max > B_H_max) {
+                printf("%d\n", B_V_max);
+            }
+            else {
+                printf("%d\n", B_H_max);
+            }
+        }
